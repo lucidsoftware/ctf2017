b9b6a3539e07f65d319baf47ce5558b9474e6caa
Nathan Cooper <nathan@lucidchart.com>
2017-03-23 14:35:26 -0600

    Modification to prevent optomizer from mangling the flag.

--- binary/mitch/basic_re/main.c
+++ binary/mitch/basic_re/main.c
@@ -3,14 +3,14 @@
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "OCDFAInspection"
 void main() {
-    const char FLAG[] = "HERE IS THE FLAG, DON'T MESS IT UP";
     printf("The rich need me. The poor have me. Don't eat me or you'll die! What am I?");
     char buf[10];
+    _Bool willBeTrue = 1;
     if(fgets(buf, sizeof buf, stdin) != NULL) {
-        if(1) {
+        if(willBeTrue) {
             printf("Wrong!");
         } else {
-            printf("The flag is %s", FLAG);
+	    printf("The flag is: HERE IS THE FLAG, DON'T MESS IT UP");
         }
     }
 }
