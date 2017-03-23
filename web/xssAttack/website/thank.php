8551b423e363b84799250a7463853d79f9464866
Bryant Nielson <bryantn@bryantn.office.lucidchart.com>
2017-03-23 16:07:39 -0600

    but WHYlsls

--- web/xssAttack/website/thank.php
+++ web/xssAttack/website/thank.php
@@ -3,7 +3,7 @@
   $userResponse = isset($_POST['value'])? $_POST['value'] : "no Response2";
   
   //loudly sweating
-  $command = "phantomjs /home/ubuntu/xssDummy/readerscript.js http://localhost/website/adminView.php?value=" . escapeshellarg($userResponse);
+  $command = "phantomjs /home/ubuntu/xssDummy/readerscript.js http://localhost/survey/adminView.php?value=" . escapeshellarg($userResponse);
   echo(shell_exec($command)); //praise the swiss army chainsaw of programming
   exit;
 ?>
